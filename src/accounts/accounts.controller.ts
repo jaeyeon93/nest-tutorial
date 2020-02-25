828e6187609f34c247d58ac6c324a39c6a32a30d
JaeyeonKIM <foodsksms@gmail.com>
2020-02-25 20:52:21 +0900

    [issue-1] HTTP 메서드정의
    - 간단하게 GET, POST, PUT, DELETE Method로 응답받고 params받는거 정의

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('accounts')
 export class AccountsController {
@@ -7,4 +7,27 @@ export class AccountsController {
     return 'GET /accounts method called';
   }
 
+  @Get(':id')
+  findById(@Param() params): string {
+    console.log(params.id);
+    return `2This action returns a ${params.id}`;
+  };
+
+  @Post()
+  createAccount(): string {
+    console.log(`request : `);
+    return `POST /accounts/`
+  }
+
+  @Put(':id')
+  updateById(@Param() params): string {
+    console.log(`updateById : ${params.id}`);
+    return `PUT /accounts/${params.id}`;
+  }
+
+  @Delete(':id')
+  deleteById(@Param() params): string {
+    console.log(`Delete by id : ${params.id}`);
+    return `DELETE /accounts/${params.id}`;
+  }
 }
