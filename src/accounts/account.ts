6d7686fbfbf218da5d2436848479e3541f6051ad
JaeyeonKIM <foodsksms@gmail.com>
2020-02-25 20:56:33 +0900

    [issue-1] Account클래스정의
    - 간단하게 id, email, password를 받는 클래스 정의

---
+++ src/accounts/account.ts
@@ -0,0 +1,19 @@
+class Account {
+  /*
+  id: string, using uuid
+  email: string, unique
+  password: string, 단방향암호화
+  access_token: string, using JWT token
+  created_at: Date, format : YYYY-MM-DD hh:mm:ss
+  updated_at: Date
+   */
+  id: string;
+  email: string;
+  password: string;
+
+  constructor(id: string, email: string, password: string) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+  }
+}
