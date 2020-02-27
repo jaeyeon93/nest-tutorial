74a18cd68c6b08675291d99ebe82246774131e10
jaeyeon kim <foodsksms@gmail.com>
2020-02-27 17:20:28 +0900

    [issue-1-temp] Account 유닛테스트작성
    - AccountTest라는 유닛테스트를 작성. Account객체가 정상적으로 생성되었는지 확인

---
+++ test/AccountTest.ts
@@ -0,0 +1,7 @@
+describe('Account Test', () => {
+  test('Account 객체 생성 테스트', () => {
+    const account: Account = new Account('uuid', 'hello@email.com', '12345');
+    expect(account.getId()).toBe('uuid');
+    expect(account.getEmail()).toBe('heelo@email.com');
+  })
+});
