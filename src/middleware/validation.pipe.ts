16e2e51f120c3c7b014d93206cc2c3144560fc94
JaeyeonKIM <foodsksms@gmail.com>
2020-03-04 22:20:32 +0900

    [issue-4] merge하기전 기타 코드수정

---
+++ src/middleware/validation.pipe.ts
@@ -0,0 +1,10 @@
+// import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+//
+//
+// @Injectable()
+// export class ValidationPipe implements PipeTransform{
+//   transform(value: any, metadata: ArgumentMetadata) {
+//     console.log(`validationPipe호출 value : ${value}`);
+//     return value;
+//   }
+// }
