6970ced245163234526a621c36e89857f38a7013
JaeyeonKIM <foodsksms@gmail.com>
2020-03-06 01:49:42 +0900

    [issue-6] AccountEntity에 Validate추가
    - password에 대한 class-validator의 경우 IsHash("알고리즘종류")를 쓰지만 bcrypt가 어떤 알고리즘을 쓰는지 몰라서 아직 못했다.

--- src/dto/responseDto.ts
+++ src/dto/responseDto.ts
@@ -2,10 +2,10 @@ import { Account } from '../account/account.entity';
 
 export class ResponseDto {
   private readonly accessToken: string;
-  private readonly created_at: string;
+  private readonly created_at: Date;
   private readonly email: string;
   private readonly id: string;
-  private readonly updated_at: string;
+  private readonly updated_at: Date;
 
   constructor(account: Account) {
     this.accessToken = account.getAccessToken();
