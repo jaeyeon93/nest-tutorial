ae86b01fb142db29069ebe96cbe256ab8304a2a0
JaeyeonKIM <foodsksms@gmail.com>
2020-03-06 00:43:35 +0900

    [issue-6] DTO validate검증
    - input으로 들어오는 DTO에 대해서 validate를 추가했다.
    input의 경우 email, password로 들어오는데 email형식인지 아닌지, password에서 최소길이 8, 대문자 하나 포함의 제약을 추가했다.

--- src/dto/account.dto.ts
+++ src/dto/account.dto.ts
@@ -1,14 +1,16 @@
 import { Account } from '../account/account.entity';
-import { IsEmail, IsString, Matches } from 'class-validator';
+import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
 
 export class AccountDto {
   private id: string;
 
   @IsString()
-  @IsEmail()
+  @IsEmail() // iput이 이메일인지 아닌지 확인
   private email: string;
 
   @IsString()
+  @MinLength(8)
+  @Matches(/(?=.*[A-Z])/) // 비밀번호 최소길이가8이고, 대문자가 최소하나 포함되있는지 확인
   private password: string;
 
   constructor(email: string, password: string) {
