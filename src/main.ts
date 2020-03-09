64b2253d543c5307fea4912ea5ed5814302dece3
JaeyeonKIM <foodsksms@gmail.com>
2020-03-09 14:48:18 +0900

    [issue-8] Swagger 적용하기전
    - swagger설정전에 정상적으로 동작하는지 확인

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './middleware/validation.pipe';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  // const options = new DocumentBuilder()
+  //   .setTitle('Jimmy API')
+  //   .setDescription('모두싸인 과제 API 문서')
+  //   .setVersion('1.0')
+  //   .addTag('sign')
+  //   .build()
+  // const document = SwaggerModule.createDocument(app, options);
+  // SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
